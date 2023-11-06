9d6ad19f73993a2112db5592bb776cce521f0c10
Joker4fun <Joker4fun1@gmail.com>
2023-11-06 04:00:01 +0300

    Initial Commit

---
+++ testApp_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testApp_SwiftUI
+//
+//  Created by Антон Казеннов on 06.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testApp_SwiftUI/testApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp_SwiftUIApp.swift
+//  testApp_SwiftUI
+//
+//  Created by Антон Казеннов on 06.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct testApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
